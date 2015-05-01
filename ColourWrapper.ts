579ba4ab4bcfc4e6ef954a4f0b0f2730189b5b01
William Gelhar <williamgelhar@gmail.com>
2015-05-01 01:49:41 -0400

    I included a lot of changes again: mostly giving the baseSamplers nice annotations and sprucing up the demo (index.html), along with changes to better match the display area and edit areas.
    I commit to committing nicer next time but if I were you I would not commit to trusting my commitment for these commitments.

--- ColourWrapper.ts
+++ ColourWrapper.ts
@@ -21,109 +21,143 @@ module ColourWrapper {
             )
     }
 
-    export var baseSamplers = {
-        colourFromWord: function(word: string, optional ?: any): HSL.HSL {
-            optional = optional || {};
-            optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
-
-            var totalHSL = word.split('').reduce(
-                function(current, char) {
-                    return current.add(
-                        hslFromSeed(char.charCodeAt(0), optional.colourModel)
-                        );
-                },
-                new HSL.HSL(0, 0, 0)
-                );
-
-            return totalHSL.scalarDivide(word.split('').length);
-        },
-        colourFromWordRandom: function(word: string, optional ?: any): HSL.HSL {
-            optional = optional || {};
-            optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
-
-            var wordSeed = word.split('').reduce(
-                function(current, char) {
-                    return current + char.charCodeAt(0);
-                },
-                0
-                );
-
-            return hslFromSeed(wordSeed, optional.colourModel);
-        },
-        colourFromVowelsInWord: function(word: string, optional ?: any): HSL.HSL {
-            optional = optional || {};
-            optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
-
-            var vowelsCount = 0;
-
-            var charCounts = word.split('').reduce(
-                function(counting, char) {
-                    if (['a', 'e', 'i', 'o', 'u'].indexOf(char) === -1) {
-                        return counting;
-                    }
-
-                    vowelsCount++;
-
-                    if (counting[char] == null || counting[char] == undefined) {
-                        counting[char] = 1
-                    } else {
-                        counting[char] = counting[char] + 1;
-                    }
-                    return counting;
-                },
-                {}
-                );
-
-            var totalHSL = Object.keys(charCounts).reduce(
-                function(current, char) {
-                    return current.add(
-                        hslFromSeed(char.charCodeAt(0), optional.colourModel).scalarMultiply(charCounts[char])
-                        );
-                },
-                new HSL.HSL(0, 0, 0)
-                );
-
-            return totalHSL.scalarDivide(vowelsCount);
-        },
-        colourFromWordRepeatsWeighted: function(word: string, optional ?: any): HSL.HSL {
-            optional = optional || {};
-            optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
-
-            var totalCount = 0;
-
-            var charCounts = word.split('').reduce(
-                function(counting, char) {
-                    if (counting[char] == null || counting[char] == undefined) {
-                        totalCount += 1;
-                        counting[char] = 1
-                    } else {
-                        totalCount += counting[char];
-                        counting[char] = counting[char] + counting[char];
-                    }
-                    return counting;
-                },
-                {}
-                );
-
-            var totalHSL = Object.keys(charCounts).reduce(
-                function(current, char) {
-                    return current.add(
-                        hslFromSeed(char.charCodeAt(0), optional.colourModel).scalarMultiply(charCounts[char])
-                        );
-                },
-                new HSL.HSL(0, 0, 0)
-                );
-
-            return totalHSL.scalarDivide(totalCount);
-        },
-        lightnessFromLength: function(word: string, maxLength ?: any): HSL.HSL {
-            if (maxLength > 0) {
-                var lightness = word.length / maxLength;
-                return new HSL.HSL(0, 0, Math.max(0, Math.min(100 * lightness, 100)));
-            } else {
-                var lightness = (maxLength - word.length) / maxLength;
-                return new HSL.HSL(0, 0, Math.max(0, Math.min(100 * lightness, 100)));
+    function annotateFunction(func, annotations: Object): Function {
+        let _func = func;
+        for (var _annotation in annotations) {
+            if (Object.prototype.hasOwnProperty.call(annotations, _annotation)) {
+                _func[_annotation] = annotations[_annotation];
             }
         }
+
+        return _func;
+    }
+
+    export var baseSamplers = {
+        colourFromWord: annotateFunction(
+            function(word: string, optional ?: any): HSL.HSL {
+                optional = optional || {};
+                optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
+
+                var totalHSL = word.split('').reduce(
+                    function(current, char) {
+                        return current.add(
+                            hslFromSeed(char.charCodeAt(0), optional.colourModel)
+                            );
+                    },
+                    new HSL.HSL(0, 0, 0)
+                    );
+
+                return totalHSL.scalarDivide(word.split('').length);
+            }, {
+                title: "Colour a word through average random colours of letters"
+            }
+        ),
+        colourFromWordRandom: annotateFunction(
+            function(word: string, optional ?: any): HSL.HSL {
+                optional = optional || {};
+                optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
+
+                var wordSeed = word.split('').reduce(
+                    function(current, char) {
+                        return current + char.charCodeAt(0);
+                    },
+                    0
+                    );
+
+                return hslFromSeed(wordSeed, optional.colourModel);
+            }, {
+                title: "Colour a word randomly"
+            }
+        ),
+        colourFromVowelsInWord: annotateFunction(
+            function(word: string, optional ?: any): HSL.HSL {
+                optional = optional || {};
+                optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
+
+                var vowelsCount = 0;
+
+                var charCounts = word.split('').reduce(
+                    function(counting, char) {
+                        if (['a', 'e', 'i', 'o', 'u'].indexOf(char.toLowerCase()) === -1) {
+                            return counting;
+                        }
+
+                        vowelsCount++;
+
+                        if (counting[char] == null || counting[char] == undefined) {
+                            counting[char] = 1
+                        } else {
+                            counting[char] = counting[char] + 1;
+                        }
+                        return counting;
+                    },
+                    {}
+                    );
+
+                var totalHSL = Object.keys(charCounts).reduce(
+                    function(current, char) {
+                        return current.add(
+                            hslFromSeed(char.charCodeAt(0), optional.colourModel).scalarMultiply(charCounts[char])
+                            );
+                    },
+                    new HSL.HSL(0, 0, 0)
+                    );
+
+                return totalHSL.scalarDivide(vowelsCount);
+            }, {
+                title: "Colour a word from the vowels in the word"
+            }
+        ),
+        colourFromWordRepeatsWeighted: annotateFunction(
+            function(word: string, optional ?: any): HSL.HSL {
+                optional = optional || {};
+                optional.colourModel = optional.colourModel || ColourModel.baseColourModels.plain;
+
+                var totalCount = 0;
+
+                var charCounts = word.split('').reduce(
+                    function(counting, char) {
+                        if (counting[char] == null || counting[char] == undefined) {
+                            totalCount += 1;
+                            counting[char] = 1
+                        } else {
+                            totalCount += counting[char];
+                            counting[char] = counting[char] + counting[char];
+                        }
+                        return counting;
+                    },
+                    {}
+                    );
+
+                var totalHSL = Object.keys(charCounts).reduce(
+                    function(current, char) {
+                        return current.add(
+                            hslFromSeed(char.charCodeAt(0), optional.colourModel).scalarMultiply(charCounts[char])
+                            );
+                    },
+                    new HSL.HSL(0, 0, 0)
+                    );
+
+                return totalHSL.scalarDivide(totalCount);
+            }, {
+                title: "Colour a word through average random colours of letters, with repeat letters weighted higher"
+            }
+        ),
+        lightnessFromLength: annotateFunction(
+            function(word: string, maxLength ?: any): HSL.HSL {
+                if (maxLength == null) {
+                    maxLength = 10;
+                }
+                if (maxLength > 0) {
+                    var lightness = word.length / maxLength;
+                    return new HSL.HSL(0, 0, Math.max(0, Math.min(100 * lightness, 100)));
+                } else {
+                    var lightness = (maxLength - word.length) / maxLength;
+                    return new HSL.HSL(0, 0, Math.max(0, Math.min(100 * lightness, 100)));
+                }
+            }, {
+                title: "Shade a word from the length of the word"
+            }
+        )
     };
 }
--- page.ts
+++ page.ts
@@ -37,69 +37,132 @@
         }
     }
 
+    var colourizing = {
+        func: function() {
+            var enterArea = document.getElementById('text-enter-area');;
+
+            var editDisplayArea = document.getElementById('text-display-area');
+
+            //Remove the old styled elements so we can add the new updatedelements cleanly
+            while (editDisplayArea.childNodes.length > 0) {
+                editDisplayArea.removeChild(editDisplayArea.childNodes[0]);
+            }
+
+            //Go through each of the input area's children and apply the colours, adding them to the display area after they are wrapped in a style
+            f.n(enterArea.childNodes).forEach(function(node) {
+                editDisplayArea.appendChild(_applyColoursRecursively(node));
+            });
+
+            _ensureSizesMatch();
+
+            //Just the function to determine what colour each word should be, up here in it's own function for visibility and to be easy to change (and to let this be dynamically changed through the page in the future)
+            function wrappingMethod(word) {
+                return colourizing.wrappingMethod(word).toCSSString();
+            }
+
+            function _ensureSizesMatch() {
+                enterArea.style.width = '';
+                enterArea.style.height = '';
+                editDisplayArea.style.width = '';
+                editDisplayArea.style.height = '';
+
+                var maxWidth = Math.max(enterArea.clientWidth, editDisplayArea.clientWidth);
+                var maxHeight = Math.max(enterArea.clientHeight, editDisplayArea.clientHeight);
+
+                enterArea.style.width = maxWidth.toString();
+                enterArea.style.height = maxHeight.toString();
+                editDisplayArea.style.width = maxWidth.toString();
+                editDisplayArea.style.height = maxHeight.toString();
+            }
+
+            //Recursively goes through an element and its children, wrapping words in an element for styling the colour of the word. Each element is cloned (not just copied or moved or what have you) to be suitable for adding elsewhere in the page
+            function _applyColoursRecursively(element) {
+                var clonedElement = element.cloneNode();
+
+                if (element.childNodes.length == 0) {
+                    if (element.data !== undefined) {
+                        var mainWrapper = document.createElement('span');
+                        var waitingWhitespace = [];
+                        element.data.split(/(\s|\u00a0)/g).forEach(function(word, index, context) {
+                            if (word === '' || /^\s|\u00a0$/g.test(word)) {
+                                waitingWhitespace.push(word);
+                                return;
+                            }
+                            if (waitingWhitespace.length > 0) {
+                                var waitingSpaces = document.createElement('span');
+                                waitingSpaces.textContent = waitingWhitespace.join('');
+                                waitingSpaces.classList.add('space-wrapper');
+                                mainWrapper.appendChild(waitingSpaces);
+                                waitingWhitespace = [];
+                            }
+
+                            var wordWrapper = document.createElement('span');
+                            wordWrapper.textContent = word;
+                            wordWrapper.style.backgroundColor = wrappingMethod(word);
+                            mainWrapper.appendChild(wordWrapper);
+                        });
+
+                        return mainWrapper;
+                    } else {
+                        return clonedElement;
+                    }
+                } else {
+                    f.n(element.childNodes).forEach(function(node) {
+                        clonedElement.appendChild(_applyColoursRecursively(node));
+                    });
+                    return clonedElement;
+                }
+            }
+        },
+        wrappingMethod: ColourWrapper.baseSamplers.colourFromWordRandom
+    };
+
     function setUpListeners(){
         var inputArea = document.getElementById('text-enter-area');
         inputArea.focus();
 
-        var safeHandler = __addFallbackOnHandler(inputArea, 'keypress', HTML5ContentEditableInputHandler(inputArea), 'keypress');
+        var safeHandler = __addFallbackOnHandler(inputArea, 'keypress', HTML5ContentEditableInputHandler(), 'keypress');
 
         inputArea.addEventListener('input', safeHandler);
         
-        function HTML5ContentEditableInputHandler (element) {
+        function HTML5ContentEditableInputHandler () {
             return function() {
-                var enterArea = element;
-
-                var editDisplayArea = document.getElementById('text-display-area');
-
-                //Remove the old styled elements so we can add the new updatedelements cleanly
-                while (editDisplayArea.childNodes.length > 0) {
-                    editDisplayArea.removeChild(editDisplayArea.childNodes[0]);
-                }
-
-                //Go through each of the input area's children and apply the colours, adding them to the display area after they are wrapped in a style
-                f.n(element.childNodes).forEach(function(node) {
-                    editDisplayArea.appendChild(_applyColoursRecursively(node));
-                });
-
-                //Just the function to determine what colour each word should be, up here in it's own function for visibility and to be easy to change (and to let this be dynamically changed through the page in the future)
-                function wrappingMethod(word) {
-                    return ColourWrapper.baseSamplers.colourFromWordRandom(word).toCSSString();
-                }
-
-                //Recursively goes through an element and its children, wrapping words in an element for styling the colour of the word. Each element is cloned (not just copied or moved or what have you) to be suitable for adding elsewhere in the page
-                function _applyColoursRecursively(element) {
-                    var clonedElement = element.cloneNode();
-
-                    if (element.childNodes.length == 0) {
-                        if (element.data !== undefined) {
-                            var mainWrapper = document.createElement('span');
-                            element.data.split(/[\s|\u00a0]/g).forEach(function(word, index, context) {
-                                var wordWrapper = document.createElement('span');
-                                wordWrapper.textContent = word;
-                                wordWrapper.style.backgroundColor = wrappingMethod(word);
-                                mainWrapper.appendChild(wordWrapper);
-
-                                if (index !== context.length) {
-                                    var space = document.createElement('span');
-                                    space.textContent = ' ';
-                                    mainWrapper.appendChild(space);
-                                }
-                            });
-
-                            return mainWrapper;
-                        } else {
-                            return clonedElement;
-                        }
-                    } else {
-                        f.n(element.childNodes).forEach(function(node) {
-                            clonedElement.appendChild(_applyColoursRecursively(node));
-                        });
-                        return clonedElement;
-                    }
-                }
+                colourizing.func();
             }
         }
+
+        document.getElementById('fill-in-text').addEventListener('click',
+            function() {
+                inputArea.textContent = document.getElementById('sample-text').textContent;
+                setTimeout(function() {
+                    colourizing.func();
+                }, 10);
+            });
+    }
+
+    function showSamplerOptions() {
+        function _samplerClick(sampler) {
+            return function(e) {
+                colourizing.wrappingMethod = sampler;
+                setTimeout(function() {
+                    colourizing.func();
+                }, 10);
+            };
+        }
+
+        var element = document.getElementById('sampler-list');
+
+        Object.keys(ColourWrapper.baseSamplers).forEach(
+            function(sampler) {
+                var samplerElem = document.createElement('li');
+                samplerElem.textContent = ColourWrapper.baseSamplers[sampler].title;
+                samplerElem.addEventListener('mouseover', _samplerClick(ColourWrapper.baseSamplers[sampler]));
+
+                element.appendChild(samplerElem);
+            }
+        )
     }
 
     setUpListeners();
+    showSamplerOptions();
 })();
