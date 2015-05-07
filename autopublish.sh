b9b2495a7eadca129bfa123c248dd53ae35fc6fc
William Gelhar <williamgelhar@gmail.com>
2015-05-06 23:46:01 -0400

    Improving autopublish

--- autopublish.sh
+++ autopublish.sh
@@ -1,3 +1,4 @@
+git stash
 git checkout gh-pages
 git pull --rebase origin gh-pages
 git merge master
@@ -6,3 +7,4 @@ git add main.js main.js.map
 git commit -m "Automatic autopublish"
 git push origin gh-pages
 git checkout master
+git stash pop
